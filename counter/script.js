0bc800a8b25de02e4e22dda4aa7028a78b23f73d
BurhanuddinA <109014084+BurhanuddinA@users.noreply.github.com>
2024-01-17 23:18:06 +0530

    Add files via upload

---
+++ counter/script.js
@@ -0,0 +1,33 @@
+
+
+let count = 0;
+const number = document.querySelector('.number');
+
+
+
+
+function plus()
+{
+count++;
+number.textContent= count;
+
+}
+
+
+function minus(){
+if(count !==0)
+{
+
+    count--;
+number.textContent = count;
+
+}
+}
+
+function reset()
+{
+
+    count = 0;
+    number.textContent = count;
+    
+}
